57112cbd38f607f69419b48fb35e13ed36060c7d
Kit Pierce <kitpierce@gmail.com>
2020-08-14 11:48:06 -0500

    Add 'find-file.sh'

---
+++ scripts/functions/find-file.sh
@@ -0,0 +1,12 @@
+function find-file () {
+    if [[ -z $1 ]];then
+        read -p "Enter file name: " PATTERN
+        printf "Searching for pattern: '$PATTERN'\n"
+        find ./ -type f -name "*$PATTERN*" | grep -i "$PATTERN" --color=always
+    else
+        for PATTERN in "$@"; do
+            printf "Searching for pattern: '$PATTERN'\n"
+            find ./ -type f -name "*$PATTERN*" | grep -i "$PATTERN" --color=always
+        done
+    fi
+}
