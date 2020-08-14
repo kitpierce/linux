18701091f9cc53b38f4be76f6fb4c4db2c31c048
Kit Pierce <kitpierce@gmail.com>
2020-08-14 11:58:12 -0500

    Setting find arg '-iname'
    Using case-insensitive name matching for 'find' command.

--- scripts/functions/find-file.sh
+++ scripts/functions/find-file.sh
@@ -2,11 +2,11 @@ function find-file () {
     if [[ -z $1 ]];then
         read -p "Enter file name: " PATTERN
         printf "Searching for pattern: '$PATTERN'\n"
-        find ./ -type f -name "*$PATTERN*" | grep -i "$PATTERN" --color=always
+        find ./ -type f -iname "*$PATTERN*" | grep -i "$PATTERN" --color=always
     else
         for PATTERN in "$@"; do
             printf "Searching for pattern: '$PATTERN'\n"
-            find ./ -type f -name "*$PATTERN*" | grep -i "$PATTERN" --color=always
+            find ./ -type f -iname "*$PATTERN*" | grep -i "$PATTERN" --color=always
         done
     fi
 }
