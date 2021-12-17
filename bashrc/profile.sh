a3c40c2f6db08cca6b3e009604c34e932a807f54
Kit Pierce <kitpierce@gmail.com>
2021-12-17 12:13:10 -0600

    Add default $PS1 (Manjaro & KDE Neon)

--- bashrc/profile.sh
+++ bashrc/profile.sh
@@ -59,3 +59,19 @@ do
 	fi
 done
 
+
+## Default PS1 Prompts For Various Linux Distros
+
+### Manjaro 21
+# root user
+# \[\033[01;31m\][\h\[\033[01;36m\] \W\[\033[01;31m\]]\$\[\033[00m\]
+
+# standard user
+# \[\033[01;32m\][\u@\h\[\033[01;37m\] \W\[\033[01;32m\]]\$\[\033[00m\]
+
+## KDE Neon (plasmashell 5.23.4 - circa 2021)
+# standard user
+# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
+
+# root user
+# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$
